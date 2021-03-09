ab8af347bbbf882b3fefd9cab5937932c91d8830
Shai Falach <shaifalach18@gmail.com>
2021-03-09 16:39:31 +0200

    added Junit To the Project

---
+++ src/unittests/Point3DTests.java
@@ -0,0 +1,12 @@
+package unittests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Point3DTests {
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+}
