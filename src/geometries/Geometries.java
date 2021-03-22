9dd788ab74d3504360101fdea5c9cf39a80fda0c
Shai Falach <shaifalach18@gmail.com>
2021-03-22 18:59:26 +0200

    added Geometries Class and GeometriesTests

--- src/geometries/Geometries.java
+++ src/geometries/Geometries.java
@@ -1,16 +1,30 @@
 package geometries;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import primitives.Point3D;
 import primitives.Ray;
 
 public class Geometries implements Intersectable {
-    public List<Intersectable> geometries;
+    private List<Intersectable> geometries_list;
+
+    public Geometries() {
+        geometries_list = new ArrayList<>();
+    }
+
+    public Geometries(Intersectable... geometries) {
+        geometries_list = List.of(geometries);
+    }
+
+    public void add(Intersectable... geometries) {
+        // TODO Auto-generated method stub
+    }
 
     @Override
     public List<Point3D> findIntersections(Ray ray) {
         // TODO Auto-generated method stub
         return null;
     }
+
 }
---
+++ src/unittests/GeometriesTests.java
@@ -0,0 +1,15 @@
+package unittests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class GeometriesTests {
+    /**
+     * Test method for {@link geometries.Geometries} Constructor.
+     */
+    @Test
+    public void testGeometries() {
+
+    }
+
+}
