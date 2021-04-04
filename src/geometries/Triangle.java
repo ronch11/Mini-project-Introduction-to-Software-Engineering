152484b9e30ace06e6b7c8d86168018db515e88e
Shai Falach <shaifalach18@gmail.com>
2021-04-05 00:03:23 +0300

    implemented Triangle find intersections

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import primitives.Point3D;
 import primitives.Ray;
+import primitives.Vector;
+
+import static primitives.Util.*;
 
 /**
  * Class point3D is the basic class representing a point in space of Euclidean
@@ -26,7 +29,40 @@ public class Triangle extends Polygon {
 
     @Override
     public List<Point3D> findIntersections(Ray ray) {
-        // TODO Auto-generated method stub
-        return super.findIntersections(ray);
+
+        List<Point3D> tentativeIntersection = plane.findIntersections(ray);
+        // if we do not intersect with plane we can not possibly intersect the triangle.
+        if (tentativeIntersection == null) {
+            return null;
+        }
+
+        // algorithm to test if given point P we got from plane findIntersections is
+        // inside the triangle.
+        Point3D p0 = ray.getP0();
+        Vector v = ray.getDir();
+        Vector v1 = vertices.get(0).subtract(p0);
+        Vector v2 = vertices.get(1).subtract(p0);
+        Vector v3 = vertices.get(2).subtract(p0);
+
+        Vector[] crossVectors = { v1.crossProduct(v2).normalize(), v2.crossProduct(v3).normalize(),
+                v3.crossProduct(v1).normalize() };
+
+        int numOfPositiveNumbers = 0;
+        for (Vector vector : crossVectors) {
+            double vn = v.dotProduct(vector);
+            if (isZero(vn)) {
+                return null;
+            }
+            if (vn > 0) {
+                numOfPositiveNumbers++;
+            }
+        }
+        // if numOfPositiveNumbers is not 0 or 3 it's mean there is at least 1 number
+        // with odd sign.
+        if (numOfPositiveNumbers != 0 && numOfPositiveNumbers != 3) {
+            return null;
+        }
+
+        return tentativeIntersection;
     }
 }
--- src/unittests/TriangleTests.java
+++ src/unittests/TriangleTests.java
@@ -14,69 +14,71 @@ import primitives.*;
  */
 public class TriangleTests {
 
-    /**
-     * Test method for {@link geometries.Triangle#getNormal(primitives.Point3D)}.
-     */
-    @Test
-    public void testGetNormal() {
-        // ============ Equivalence Partitions Tests ==============
-        // TC01: There is a simple single test here
-        Triangle triangle = new Triangle(new Point3D(0, 0, 1), new Point3D(1, 0, 0), new Point3D(0, 1, 0));
-        double sqrt3 = Math.sqrt(1d / 3);
-        assertEquals("Bad normal to triangle", new Vector(sqrt3, sqrt3, sqrt3),
-                triangle.getNormal(new Point3D(0, 0, 1)));
-    }
+        /**
+         * Test method for {@link geometries.Triangle#getNormal(primitives.Point3D)}.
+         */
+        @Test
+        public void testGetNormal() {
+                // ============ Equivalence Partitions Tests ==============
+                // TC01: There is a simple single test here
+                Triangle triangle = new Triangle(new Point3D(0, 0, 1), new Point3D(1, 0, 0), new Point3D(0, 1, 0));
+                double sqrt3 = Math.sqrt(1d / 3);
+                assertEquals("Bad normal to triangle", new Vector(sqrt3, sqrt3, sqrt3),
+                                triangle.getNormal(new Point3D(0, 0, 1)));
+        }
 
-    /**
-     * Test method for
-     * {@link geometries.Triangle#findIntersections(primitives.Ray)}. This test does
-     * not try to check for rays that does not cross the Plane of the triangle those
-     * cases are covered by the plane tests.
-     */
-    @Test
-    public void testFindIntersections() {
-        // setup
-        Point3D p0 = new Point3D(2, 0, 2);
-        Point3D t1 = new Point3D(-5, 2, 1);
-        Point3D t2 = new Point3D(5, 2, 1);
-        Point3D t3 = new Point3D(0, 6, 6);
-        Triangle triangle = new Triangle(t1, t2, t3);
+        /**
+         * Test method for
+         * {@link geometries.Triangle#findIntersections(primitives.Ray)}. This test does
+         * not try to check for rays that does not cross the Plane of the triangle those
+         * cases are covered by the plane tests.
+         */
+        @Test
+        public void testFindIntersections() {
+                // setup
+                Point3D p0 = new Point3D(2, 0, 2);
+                Point3D t1 = new Point3D(-5, 2, 1);
+                Point3D t2 = new Point3D(5, 2, 1);
+                Point3D t3 = new Point3D(0, 6, 6);
+                Triangle triangle = new Triangle(t1, t2, t3);
 
-        // ============ Equivalence Partitions Tests ==============
-        // **** Group: Ray intersect the plane of the triangle:
-        // TC01: Ray intersect the plane of the triangle inside the triangle
-        Ray r1 = new Ray(p0, new Vector(0, 1, 0));
+                // ============ Equivalence Partitions Tests ==============
+                // **** Group: Ray intersect the plane of the triangle:
+                // TC01: Ray intersect the plane of the triangle inside the triangle
+                Ray r1 = new Ray(p0, new Vector(0, 1, 0));
 
-        List<Point3D> actualValue = triangle.findIntersections(r1);
-        assertEquals("TC01: The ray should intersected the triangle once.", 1, actualValue.size());
+                List<Point3D> actualValue = triangle.findIntersections(r1);
+                assertEquals("TC01: The ray should intersected the triangle once.", 1, actualValue.size());
 
-        assertEquals("TC01: Wrong values in the intersection point.", List.of(new Point3D(2, 2.8, 2)), actualValue);
+                assertEquals("TC01: Wrong values in the intersection point.", List.of(new Point3D(2, 2.8, 2)),
+                                actualValue);
 
-        // TC02: Ray intersect the plane of the triangle outside the triangle(against
-        // edge)
-        assertNull("TC02: triangle should not intersected by Ray.",
-                triangle.findIntersections(new Ray(p0, new Vector(1, 1, 0))));
+                // TC02: Ray intersect the plane of the triangle outside the triangle(against
+                // edge)
+                assertNull("TC02: triangle should not intersected by Ray.",
+                                triangle.findIntersections(new Ray(p0, new Vector(1, 1, 0))));
 
-        // TC03: Ray intersect the plane of the triangle outside the triangle(against
-        // vertex)
-        assertNull("TC02: triangle should not intersected by Ray.",
-                triangle.findIntersections(new Ray(p0, new Vector(6, 1.6, 0.5))));
-        // =============== Boundary Values Tests ==================
-        // TC04: Ray intersect Triangle on edge
-        Ray r2 = new Ray(p0, new Vector(0.5, 4, 1.5));
+                // TC03: Ray intersect the plane of the triangle outside the triangle(against
+                // vertex)
+                assertNull("TC02: triangle should not intersected by Ray.",
+                                triangle.findIntersections(new Ray(p0, new Vector(6, 1.6, 0.5))));
+                // =============== Boundary Values Tests ==================
+                // TC04: Ray intersect Triangle on edge
+                Ray r2 = new Ray(p0, new Vector(0.5, 4, 1.5));
 
-        actualValue = triangle.findIntersections(r2);
-        assertEquals("TC04: The ray should intersected the triangle once.", 1, actualValue.size());
+                actualValue = triangle.findIntersections(r2);
+                assertEquals("TC04: The ray should intersected the triangle once.", 1, actualValue.size());
 
-        assertEquals("TC04: Wrong values in the intersection point.", List.of(new Point3D(2.5, 4, 3.5)), actualValue);
+                assertEquals("TC04: Wrong values in the intersection point.", List.of(new Point3D(2.5, 4, 3.5)),
+                                actualValue);
 
-        // TC05: Ray intersect Triangle on vertex
-        assertNull("TC05: triangle should not intersected by Ray.",
-                triangle.findIntersections(new Ray(p0, new Vector(3, 2, -1))));
+                // TC05: Ray intersect Triangle on vertex
+                assertNull("TC05: triangle should not intersected by Ray.",
+                                triangle.findIntersections(new Ray(p0, new Vector(3, 2, -1))));
 
-        // TC06: Ray intersect on plane on line continuation of the Triangle edge
-        assertNull("TC06: triangle should not intersected by Ray.",
-                triangle.findIntersections(new Ray(p0, new Vector(4, 2, -1))));
+                // TC06: Ray intersect on plane on line continuation of the Triangle edge
+                assertNull("TC06: triangle should not intersected by Ray.",
+                                triangle.findIntersections(new Ray(p0, new Vector(4, 2, -1))));
 
-    }
+        }
 }
