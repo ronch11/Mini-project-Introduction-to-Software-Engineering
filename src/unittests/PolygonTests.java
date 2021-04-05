601a59b8738ab2727601dbd46cd1c90c7d75f279
Shai Falach <shaifalach18@gmail.com>
2021-04-05 21:28:49 +0300

    added test cases for polygon findIntersections

--- src/unittests/PolygonTests.java
+++ src/unittests/PolygonTests.java
@@ -1,6 +1,9 @@
 package unittests;
 
 import static org.junit.Assert.*;
+
+import java.util.List;
+
 import org.junit.Test;
 
 import geometries.*;
@@ -85,7 +88,52 @@ public class PolygonTests {
         Polygon pl = new Polygon(new Point3D(0, 0, 1), new Point3D(1, 0, 0), new Point3D(0, 1, 0),
                 new Point3D(-1, 1, 1));
         double sqrt3 = Math.sqrt(1d / 3);
-        assertEquals("Bad normal to triangle", new Vector(sqrt3, sqrt3, sqrt3), pl.getNormal(new Point3D(0, 0, 1)));
+        assertEquals("Bad normal to polygon", new Vector(sqrt3, sqrt3, sqrt3), pl.getNormal(new Point3D(0, 0, 1)));
     }
 
+    /**
+     * Test method for {@link geometries.Polygon#findIntersections(primitives.Ray)}.
+     * This test does not try to check for rays that does not cross the Plane of the
+     * polygon those cases are covered by the plane tests.
+     */
+    @Test
+    public void testFindIntersections() {
+        // setup
+        Polygon pl = new Polygon(new Point3D(0, 0, 4), new Point3D(4, 0, 0), new Point3D(0, 4, 0),
+                new Point3D(-4, 4, 4));
+        Point3D p0 = new Point3D(-1, 0, 1);
+
+        // ============ Equivalence Partitions Tests ==============
+        // **** Group: Ray intersect the plane of the polygon:
+        // TC01: Ray intersect the plane of the polygon inside the polygon
+        Ray r1 = new Ray(p0, new Vector(1, 1, 0));
+
+        List<Point3D> actualValue = pl.findIntersections(r1);
+        assertEquals("TC01: The ray should intersected the polygon once.", 1, actualValue.size());
+
+        assertEquals("TC01: Wrong values in the intersection point.", List.of(new Point3D(1, 2, 1)), actualValue);
+
+        // TC02: Ray intersect the plane of the polygon outside the polygon(against
+        // edge)
+        assertNull("TC02: polygon should not intersected by Ray.",
+                pl.findIntersections(new Ray(p0, new Vector(1, 1, 10))));
+
+        // TC03: Ray intersect the plane of the polygon outside the polygon(against
+        // vertex)
+        assertNull("TC02: polygon should not intersected by Ray.",
+                pl.findIntersections(new Ray(p0, new Vector(1, -1, 4))));
+        // =============== Boundary Values Tests ==================
+        // TC04: Ray intersect Polygon on edge
+        Ray r2 = new Ray(p0, new Vector(-0.5, 1.5, 3));
+        assertNull("TC04: The ray should intersected the polygon once.", pl.findIntersections(r2));
+
+        // TC05: Ray intersect Polygon on vertex
+        assertNull("TC05: polygon should not intersected by Ray.",
+                pl.findIntersections(new Ray(p0, new Vector(1, 0, 3))));
+
+        // TC06: Ray intersect on plane on line continuation of the Polygon edge
+        assertNull("TC06: polygon should not intersected by Ray.",
+                pl.findIntersections(new Ray(p0, new Vector(3, -2, 3))));
+
+    }
 }
