0939f70029e9541e7a42e29214ec7c3dcb689c47
ron hodadedi <ronchaim318@gmail.com>
2021-04-19 22:47:27 +0300

    added java doc for Geometries

--- src/geometries/Geometries.java
+++ src/geometries/Geometries.java
@@ -6,17 +6,38 @@ import java.util.List;
 import primitives.Point3D;
 import primitives.Ray;
 
+/**
+ * Geometries class represents compsition of Geometries in 3D Cartesian
+ * coordinate system
+ * 
+ * @author SHAI FALACH and RON HAIM HODADEDI
+ */
 public class Geometries implements Intersectable {
     private List<Intersectable> geometriesList;
 
+    /**
+     * A Constructor the sets the objevct to be with empty list of intersectable
+     * shapes.
+     */
     public Geometries() {
         geometriesList = new ArrayList<>();
     }
 
+    /**
+     * A Constructor the gets an Array of intersectable Geometries shapes. and
+     * create a composite list of them.
+     * 
+     * @param geometries
+     */
     public Geometries(Intersectable... geometries) {
         geometriesList = List.of(geometries);
     }
 
+    /**
+     * Add a new Shape/s to the List.
+     * 
+     * @param geometries - array of Intersectable Geometries.
+     */
     public void add(Intersectable... geometries) {
         geometriesList.addAll(List.of(geometries));
     }
