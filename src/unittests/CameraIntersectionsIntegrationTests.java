10ae6166de55f57d70fae93ae925ee3b0c5f341c
Shai Falach <shaifalach18@gmail.com>
2021-05-04 10:29:41 +0300

    fixed integration tests

--- src/unittests/CameraIntersectionsIntegrationTests.java
+++ src/unittests/CameraIntersectionsIntegrationTests.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.junit.Test;
 
 import elements.Camera;
+import geometries.Intersectable;
 import geometries.Plane;
 import geometries.Sphere;
 import geometries.Triangle;
@@ -23,13 +24,13 @@ import primitives.Vector;
 public class CameraIntersectionsIntegrationTests {
 
     /**
-     * test Sphere Plane and Triangle intersections with Camera Rays.
+     * integration test of
+     * {@link elements.Camera#constructRayThroughPixel(int, int, int, int)} and
+     * {@link geometries.Sphere#findIntersections(Ray)}
      */
     @Test
-    public void testCameraFindIntersection() {
+    public void testCameraAndSphere() {
         // setup constants
-        int pixelNumberOfCols = 3;
-        int pixelNumberOfRows = 3;
         double distance = 1d;
         double vpWidth = 3d;
         double vpHeight = 3d;
@@ -42,17 +43,8 @@ public class CameraIntersectionsIntegrationTests {
         camera.setViewPlaneDistance(distance);
         camera.setViewPlaneSize(vpHeight, vpWidth);
 
-        for (int i = 0; i < vpHeight; i++) {
-            for (int j = 0; j < vpWidth; j++) {
-                Ray cameraRay = camera.constructRayThroughPixel(pixelNumberOfCols, pixelNumberOfRows, j, i);
-                List<Point3D> actualValue = sphere.findIntersections(cameraRay);
-                if (i == 1 && j == 1) {
-                    assertEquals("TC01: The Center Ray should intersect the sphere 2 times.", 2, actualValue.size());
-                } else {
-                    assertNull("TC01: Only the center ray should intersect the sphere.", actualValue);
-                }
-            }
-        }
+        int actualValue = findAllIntersections(camera, sphere);
+        assertEquals("TC01: The Center Ray should intersect the sphere 2 times.", 2, actualValue);
 
         // TC02: all ray from camera through View Panel intersect twice.
         sphere = new Sphere(new Point3D(0, 0, -2.5), 2.5);
@@ -60,122 +52,119 @@ public class CameraIntersectionsIntegrationTests {
         camera.setViewPlaneDistance(distance);
         camera.setViewPlaneSize(vpHeight, vpWidth);
 
-        for (int i = 0; i < vpHeight; i++) {
-            for (int j = 0; j < vpWidth; j++) {
-                Ray cameraRay = camera.constructRayThroughPixel(pixelNumberOfCols, pixelNumberOfRows, j, i);
-                List<Point3D> actualValue = sphere.findIntersections(cameraRay);
-                assertEquals("TC02: All Rays should intersect the sphere 2 times.", 2, actualValue.size());
-            }
-        }
+        actualValue = findAllIntersections(camera, sphere);
+        assertEquals("TC02: All Rays should intersect the sphere 2 times.", 18, actualValue);
 
         // TC03: all Ray but Corner Rays intersect the Sphere.
         sphere = new Sphere(new Point3D(0, 0, -2), 2);
 
-        for (int i = 0; i < vpHeight; i++) {
-            for (int j = 0; j < vpWidth; j++) {
-                Ray cameraRay = camera.constructRayThroughPixel(pixelNumberOfCols, pixelNumberOfRows, j, i);
-                List<Point3D> actualValue = sphere.findIntersections(cameraRay);
-                if (i == 1 || j == 1) { // if it's not a corner ray
-                    assertEquals("TC03: The Center Ray should intersect the sphere 2 times.", 2, actualValue.size());
-                } else {
-                    assertNull("TC03: Corner rays should not intersect the sphere.", actualValue);
-                }
-            }
-        }
+        actualValue = findAllIntersections(camera, sphere);
+        assertEquals("TC03: The non corner Rays should intersect the sphere 2 times.", 10, actualValue);
 
         // TC04: camera inside Sphere so only 9 intersection (1 for each pixel).
         sphere = new Sphere(new Point3D(0, 0, -2), 4);
 
-        for (int i = 0; i < vpHeight; i++) {
-            for (int j = 0; j < vpWidth; j++) {
-                Ray cameraRay = camera.constructRayThroughPixel(pixelNumberOfCols, pixelNumberOfRows, j, i);
-                List<Point3D> actualValue = sphere.findIntersections(cameraRay);
-                assertEquals("TC04: All Rays should intersect the sphere one time only.", 1, actualValue.size());
-            }
-        }
+        actualValue = findAllIntersections(camera, sphere);
+        assertEquals("TC04: All Rays should intersect the sphere one time only.", 9, actualValue);
 
         // TC05: no intersections because camera is after the Sphere.
         sphere = new Sphere(new Point3D(0, 0, 1), 0.5);
 
-        for (int i = 0; i < vpHeight; i++) {
-            for (int j = 0; j < vpWidth; j++) {
-                Ray cameraRay = camera.constructRayThroughPixel(pixelNumberOfCols, pixelNumberOfRows, j, i);
-                List<Point3D> actualValue = sphere.findIntersections(cameraRay);
-                assertNull("TC05: All rays should not intersect the sphere.", actualValue);
-            }
-        }
+        actualValue = findAllIntersections(camera, sphere);
+        assertEquals("TC05: All rays should not intersect the sphere.", 0, actualValue);
 
-        // TC06: plane is perpendicular to the Camera Vto Ray.
+    }
+
+    /**
+     * integration test of
+     * {@link elements.Camera#constructRayThroughPixel(int, int, int, int)} and
+     * {@link geometries.Plane#findIntersections(Ray)}
+     */
+    @Test
+    public void testCameraAndPlane() {
+        // setup constants
+        double distance = 1d;
+        double vpWidth = 3d;
+        double vpHeight = 3d;
+        Vector camVTo = new Vector(0, 0, -1);
+        Vector camVUp = new Vector(0, 1, 0);
+        Camera camera = new Camera(Point3D.ZERO, camVTo, camVUp);
+        camera.setViewPlaneDistance(distance);
+        camera.setViewPlaneSize(vpHeight, vpWidth);
+
+        // TC01: plane is perpendicular to the Camera Vto Ray.
         Plane plane = new Plane(new Point3D(0, 0, -1), camVTo);
+        int actualValue = findAllIntersections(camera, plane);
+        assertEquals("TC01: All Rays should intersect the Plane one time only.", 9, actualValue);
 
-        for (int i = 0; i < vpHeight; i++) {
-            for (int j = 0; j < vpWidth; j++) {
-                Ray cameraRay = camera.constructRayThroughPixel(pixelNumberOfCols, pixelNumberOfRows, j, i);
-                List<Point3D> actualValue = plane.findIntersections(cameraRay);
-                assertEquals("TC06: All Rays should intersect the Plane one time only.", 1, actualValue.size());
-            }
-        }
-
-        // TC07: Plane is diagonally aligned with camera but still all 9 pixels
+        // TC02: Plane is diagonally aligned with camera but still all 9 pixels
         // intersects it.
         plane = new Plane(new Point3D(0, 0, -1), new Vector(0, 1, 5));
 
-        for (int i = 0; i < vpHeight; i++) {
-            for (int j = 0; j < vpWidth; j++) {
-                Ray cameraRay = camera.constructRayThroughPixel(pixelNumberOfCols, pixelNumberOfRows, j, i);
-                List<Point3D> actualValue = plane.findIntersections(cameraRay);
-                assertEquals("TC07: All Rays should intersect the Plane one time only.", 1, actualValue.size());
-            }
-        }
+        actualValue = findAllIntersections(camera, plane);
+        assertEquals("TC02: All Rays should intersect the Plane one time only.", 9, actualValue);
 
-        // TC08: Plane is intersected by Rays in the first 2 rows (last row does not
+        // TC03: Plane is intersected by Rays in the first 2 rows (last row does not
         // intersect).
         plane = new Plane(new Point3D(0, 0, -2), new Vector(0, -1, 1));
 
-        for (int i = 0; i < vpHeight; i++) {
-            for (int j = 0; j < vpWidth; j++) {
-                Ray cameraRay = camera.constructRayThroughPixel(pixelNumberOfCols, pixelNumberOfRows, j, i);
-                List<Point3D> actualValue = plane.findIntersections(cameraRay);
-                if (i <= 1) {
-                    assertEquals("TC08: Rays in first and second row should intersect the plane one time only.", 1,
-                            actualValue.size());
-                } else {
-                    assertNull("TC08: Last row rays should not intersect the plane.", actualValue);
-                }
-            }
-        }
+        actualValue = findAllIntersections(camera, plane);
+        assertEquals("TC03: Rays in first and second row should intersect the plane one time only.", 6, actualValue);
+    }
 
-        // TC09: Triangle should be intersected only by the center pixel.
+    /**
+     * integration test of
+     * {@link elements.Camera#constructRayThroughPixel(int, int, int, int)} and
+     * {@link geometries.Triangle#findIntersections(Ray)}
+     */
+    @Test
+    public void testCameraAndTriangle() {
+        // setup constants
+        double distance = 1d;
+        double vpWidth = 3d;
+        double vpHeight = 3d;
+        Vector camVTo = new Vector(0, 0, -1);
+        Vector camVUp = new Vector(0, 1, 0);
+        Camera camera = new Camera(Point3D.ZERO, camVTo, camVUp);
+        camera.setViewPlaneDistance(distance);
+        camera.setViewPlaneSize(vpHeight, vpWidth);
+
+        // TC01: Triangle should be intersected only by the center pixel.
         Triangle triangle = new Triangle(new Point3D(0, 1, -2), new Point3D(1, -1, -2), new Point3D(-1, -1, -2));
+        int actualValue = findAllIntersections(camera, triangle);
+        assertEquals("TC01: Only the Center Ray should intersect the triangle once.", 1, actualValue);
 
-        for (int i = 0; i < vpHeight; i++) {
-            for (int j = 0; j < vpWidth; j++) {
-                Ray cameraRay = camera.constructRayThroughPixel(pixelNumberOfCols, pixelNumberOfRows, j, i);
-                List<Point3D> actualValue = triangle.findIntersections(cameraRay);
-                if (i == 1 && j == 1) {
-                    assertEquals("TC09: The Center Ray should intersect the triangle once.", 1, actualValue.size());
-                } else {
-                    assertNull("TC09: Only the center ray should intersect the triangle.", actualValue);
-                }
-            }
-        }
-
-        // TC10: long triangle but narrow base so only center and upper rays intersect
+        // TC02: long triangle but narrow base so only center and upper rays intersect
         // with it.
         triangle = new Triangle(new Point3D(0, 20, -2), new Point3D(1, -1, -2), new Point3D(-1, -1, -2));
 
+        actualValue = findAllIntersections(camera, triangle);
+        assertEquals("TC02: The center/upper Ray should intersect the triangle once.", 2, actualValue);
+    }
+
+    /**
+     * A helper function to find all intersection point of camera view panel rays
+     * and an intersectable shape
+     * 
+     * @param camera Camera - a camera we want to find intersection with.
+     * @param shape  Intersectable - a Geometry that has implement the Intersectable
+     *               interface.
+     * @return Int - total number of intersection points.
+     */
+    private int findAllIntersections(Camera camera, Intersectable shape) {
+        int pixelNumberOfCols = 3;
+        int pixelNumberOfRows = 3;
+        double vpWidth = 3d;
+        double vpHeight = 3d;
+
+        int sumOfIntersection = 0;
         for (int i = 0; i < vpHeight; i++) {
             for (int j = 0; j < vpWidth; j++) {
                 Ray cameraRay = camera.constructRayThroughPixel(pixelNumberOfCols, pixelNumberOfRows, j, i);
-                List<Point3D> actualValue = triangle.findIntersections(cameraRay);
-                if ((i == 1 || i == 0) && j == 1) {
-                    assertEquals("TC10: The center/upper Ray should intersect the triangle once.", 1,
-                            actualValue.size());
-                } else {
-                    assertNull("TC10: Only the center/upper ray should intersect the triangle.", actualValue);
-                }
+                List<Point3D> actualValue = shape.findIntersections(cameraRay);
+                sumOfIntersection += actualValue == null ? 0 : actualValue.size();
             }
         }
-
+        return sumOfIntersection;
     }
 }
