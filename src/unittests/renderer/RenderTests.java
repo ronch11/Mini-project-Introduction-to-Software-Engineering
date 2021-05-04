9213aa85ada9ec6bd828a13c215c15a97a3902cf
Shai Falach <shaifalach18@gmail.com>
2021-05-04 16:59:04 +0300

    added Tests for LIghts

--- src/unittests/renderer/RenderTests.java
+++ src/unittests/renderer/RenderTests.java
@@ -72,4 +72,33 @@ public class RenderTests {
 		render.writeToImage();
 	}
 
+	@Test
+	public void basicRenderMultiColorTest() {
+		Scene scene = new Scene("Test scene")//
+				.setAmbientLight(new AmbientLight(new Color(java.awt.Color.WHITE), 0.2)); //
+
+		scene.geometries.add(new Sphere(new Point3D(0, 0, -100), 50), //
+				new Triangle(new Point3D(-100, 0, -100), new Point3D(0, 100, -100), new Point3D(-100, 100, -100)) // up
+																													// left
+						.setEmission(new Color(java.awt.Color.GREEN)),
+				new Triangle(new Point3D(100, 0, -100), new Point3D(0, 100, -100), new Point3D(100, 100, -100)), // up
+																													// right
+				new Triangle(new Point3D(-100, 0, -100), new Point3D(0, -100, -100), new Point3D(-100, -100, -100)) // down
+																													// left
+						.setEmission(new Color(java.awt.Color.RED)),
+				new Triangle(new Point3D(100, 0, -100), new Point3D(0, -100, -100), new Point3D(100, -100, -100)) // down
+																													// right
+						.setEmission(new Color(java.awt.Color.BLUE)));
+
+		ImageWriter imageWriter = new ImageWriter("color render test", 1000, 1000);
+		Render render = new Render() //
+				.setWriter(imageWriter) //
+				.setScene(scene) //
+				.setCamera(camera) //
+				.setRayTracer(new RayTracerBasic(scene));
+
+		render.renderImage();
+		render.printGrid(100, new Color(java.awt.Color.WHITE));
+		render.writeToImage();
+	}
 }
