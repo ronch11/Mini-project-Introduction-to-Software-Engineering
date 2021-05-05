42e9cd3c9d6244440cbe73376a6ebcf8de44f6ea
Shai Falach <shaifalach18@gmail.com>
2021-05-05 22:10:59 +0300

    added Light abstract class

---
+++ src/elements/Light.java
@@ -0,0 +1,28 @@
+package elements;
+
+import primitives.Color;
+
+/**
+ * An abstract class that give the common field for all light classes.
+ */
+abstract class Light {
+    private final Color intensity;
+
+    /**
+     * default constructor for all light based classes.
+     * 
+     * @param intensity
+     */
+    protected Light(Color intensity) {
+        this.intensity = intensity;
+    }
+
+    /**
+     * A Getter for the private field intensity
+     * 
+     * @return Color - The Color of light.
+     */
+    public Color getIntensity() {
+        return intensity;
+    }
+}
