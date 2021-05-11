4696e0535c2ff3fa7286d35c1803104dfaf6dfdd
Shai Falach <shaifalach18@gmail.com>
2021-05-11 18:52:43 +0300

    implemented getDistance  to all lightsources

--- src/elements/PointLight.java
+++ src/elements/PointLight.java
@@ -69,4 +69,9 @@ public class PointLight extends Light implements LightSource {
     public Vector getL(Point3D p) {
         return p.subtract(position).normalize();
     }
+
+    @Override
+    public double getDistance(Point3D point) {
+        return alignZero(point.distance(position));
+    }
 }
