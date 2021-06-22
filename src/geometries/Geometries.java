4a5dddd3dd2915d364e7ca52f1daa790f861a2cb
Shai Falach <shaifalach18@gmail.com>
2021-06-22 10:24:02 +0300

    improved AABB instesection check

--- src/geometries/Geometries.java
+++ src/geometries/Geometries.java
@@ -178,6 +178,18 @@ public class Geometries implements Intersectable {
         }
     }
 
+    private boolean isOverlapping(double minCoord, double maxCoord, double pointCoord, double dirInverted) {
+        double min, max;
+        if (dirInverted > 0) {
+            min = alignZero((minCoord - pointCoord) * dirInverted);
+            max = alignZero((maxCoord - pointCoord) * dirInverted);
+        } else {
+            min = alignZero((maxCoord - pointCoord) * dirInverted);
+            max = alignZero((minCoord - pointCoord) * dirInverted);
+        }
+        return !(min > max);
+    }
+
     /**
      * check if a ray is intersecting with the AABB of the geometry shape.
      *
@@ -185,29 +197,43 @@ public class Geometries implements Intersectable {
      * @return
      */
     private boolean isRayIntersectingAABB(Ray ray) {
-        Point3D min = boundingBox.getMinLocation();
-        Point3D max = boundingBox.getMaxLocation();
         Point3D p0 = ray.getP0();
         Point3D dirHead = ray.getDir().getHead();
+        Point3D min = boundingBox.getMinLocation();
+        Point3D max = boundingBox.getMaxLocation();
 
         // in order to optimize and avoid div by 0 error we will use the inverted
         // values.
-        double dirInvertedX = 1d / dirHead.getX();
-        double dirInvertedY = 1d / dirHead.getY();
-        double dirInvertedZ = 1d / dirHead.getZ();
+        double dirInvertedX = alignZero(1d / dirHead.getX());
+        double dirInvertedY = alignZero(1d / dirHead.getY());
+        double dirInvertedZ = alignZero(1d / dirHead.getZ());
 
+        if (!isOverlapping(min.getX(), max.getX(), p0.getX(), dirInvertedX)) {
+            return false;
+        }
+
+        if (!isOverlapping(min.getY(), max.getY(), p0.getY(), dirInvertedY)) {
+            return false;
+        }
+
+        if (!isOverlapping(min.getZ(), max.getZ(), p0.getZ(), dirInvertedZ)) {
+            return false;
+        }
+        return true;
         // calculate distance between min/max and p0 in 3D dimensions separately:
-        double xMin = alignZero((min.getX() - p0.getX()) * dirInvertedX);
-        double xMax = alignZero((max.getX() - p0.getX()) * dirInvertedX);
-        double yMin = alignZero((min.getY() - p0.getY()) * dirInvertedY);
-        double yMax = alignZero((max.getY() - p0.getY()) * dirInvertedY);
-        double zMin = alignZero((min.getZ() - p0.getZ()) * dirInvertedZ);
-        double zMax = alignZero((max.getZ() - p0.getZ()) * dirInvertedZ);
+        // double xMin = alignZero((min.getX() - p0.getX()) * dirInvertedX);
+        // double xMax = alignZero((max.getX() - p0.getX()) * dirInvertedX);
+        // double yMin = alignZero((min.getY() - p0.getY()) * dirInvertedY);
+        // double yMax = alignZero((max.getY() - p0.getY()) * dirInvertedY);
+        // double zMin = alignZero((min.getZ() - p0.getZ()) * dirInvertedZ);
+        // double zMax = alignZero((max.getZ() - p0.getZ()) * dirInvertedZ);
 
         // pick the maximum between minimum values in the 3D dimensions.
-        double tMin = Math.max(Math.max(Math.min(xMin, xMax), Math.min(yMin, yMax)), Math.min(zMin, zMax));
+        // double tMin = Math.max(Math.max(Math.min(xMin, xMax), Math.min(yMin, yMax)),
+        // Math.min(zMin, zMax));
         // pick the minimum between the maximum values in the 3D dimensions.
-        double tMax = Math.min(Math.min(Math.max(xMin, xMax), Math.max(yMin, yMax)), Math.max(zMin, zMax));
+        // double tMax = Math.min(Math.min(Math.max(xMin, xMax), Math.max(yMin, yMax)),
+        // Math.max(zMin, zMax));
 
         // if tMax < 0, ray (line) is intersecting AABB, but the whole AABB is behind
         // us
@@ -215,6 +241,6 @@ public class Geometries implements Intersectable {
         // which in both cases we return false.
         // else, ray intersecting with the Axis Aligned Bounding Box and we return
         // true.
-        return !(tMax < 0 || tMin > tMax);
+        // return !(tMax < 0 || tMin > tMax);
     }
 }
